d2731a3ccecc4addda02d8070e07000d70399f96
Erik Frojdh <erik.frojdh@gmail.com>
2018-05-17 11:28:00 +0200

    initial

---
+++ build.sh
@@ -0,0 +1,22 @@
+# set -x
+
+# declare -a CMAKE_PLATFORM_FLAGS
+# if [[ ${HOST} =~ .*linux.* ]]; then
+#     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+# fi
+
+
+mkdir build
+cd build
+echo $PREFIX
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DUSE_TEXTCLIENT=OFF \
+      -DUSE_RECEIVER=OFF \
+      -DUSE_GUI=ON \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DUSE_HDF5=OFF\
+     
+
+cmake --build . -- -j8
+cmake --build . --target install
