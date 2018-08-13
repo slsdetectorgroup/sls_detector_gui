e7f5dca95fd79ad6a42c92d5da2dae25273de92f
Erik Frojdh <erik.frojdh@gmail.com>
2018-08-13 16:21:38 +0200

    patch for png

--- build.sh
+++ build.sh
@@ -1,22 +1,11 @@
-# set -x
-
-# declare -a CMAKE_PLATFORM_FLAGS
-# if [[ ${HOST} =~ .*linux.* ]]; then
-#     CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-# fi
-
-
 mkdir build
 cd build
-echo $PREFIX
-echo "------------- prefix----------"
-echo $CONDA_PREFIX
 
 cmake .. \
       -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DUSE_TEXTCLIENT=OFF \
-      -DUSE_RECEIVER=OFF \
+      -DUSE_TEXTCLIENT=ON \
+      -DUSE_RECEIVER=ON \
       -DUSE_GUI=ON \
       -DCMAKE_BUILD_TYPE=Release \
       -DUSE_HDF5=OFF\
